27aba4a7f1e312f224c90063bbd542fae48e58e2
lorenzo-cavazzi <lorenzo.cavazzi.tech@gmail.com>
2018-09-23 01:13:30 +0200

    added overlay_channels function, processing.py helper functions, default image set to test

--- bblab/image/processing.py
+++ bblab/image/processing.py
@@ -1,12 +1,26 @@
 """
 This module contains functions to analyze single channel images from microscopes.
 """
-
-# use Google notation: https://github.com/google/styleguide/blob/gh-pages/pyguide.md
-# this is the alternative notation: https://numpydoc.readthedocs.io/en/latest/format.html#docstring-standard
-
-
+import os
+import logging
+import numpy
+import cv2
 from pathlib import Path
+
+# TODO: Logging should be implemented at module level. It has temporary been added
+# to already use logging properly while writing the first release
+log = logging.getLogger("bblab")
+
+
+def set_log_level(level = logging.WARNING):
+    """Define log level. Please consider this solution as temporary.
+
+     Args:
+        level (int): loggging level. Please use logging.DEBUG .INFO .WARNING
+            .ERROR .CRITICAL from the `logging` module.
+    """
+    log.setLevel(level)
+
 
 def overlay_channels(input_folder = None, output_folder = None, return_image = False,
         maximize_intensity = True, **kwargs):
@@ -65,9 +79,75 @@ def overlay_channels(input_folder = None, output_folder = None, return_image = F
         >>> overlay_channels(r_channel_multiplier = 0.5, b_channel_multiplier = 5)
 	"""
 
-    print("overlay_channels stub")
-    # remember to catch errors and write some feedback to the standard output
-    raise NotImplementedError
+    # check input files
+    if "input_files" in kwargs:
+        files = _get_validated_filenames(kwargs["input_files"])
+    else:
+        if input_folder is None:
+            input_folder = "."
+        files = _get_filenames_from_folder(input_folder)
+        files.sort() # let's make RGB association deterministic for equal sets of names
+    if len(files) != 3:
+        raise ValueError("3 files expected, {num} found").format(num=str(len(files)))
+    extensions = set([file.suffix for file in files])
+    # TODO: the following is NOT a good way to test file format. A Tiff image may not have
+    # .tiff suffix, an a .tiff suffix doesn't mean file is a Tiff image. This is a
+    # simplification but it also enforce to use proper file extensions
+    if ".tiff" not in extensions or len(extensions) > 1:
+        raise ValueError("only .tiff files are accepted").format(num=str(len(files)))
+    log.info("working on the following input files: {names}".format(names=[str(file.absolute()) for file in files]))
+
+    # get files, apply transformations (where needed) and create RGB image
+    channels = []
+    for file in files:
+        channel = cv2.imread(str(file), cv2.IMREAD_UNCHANGED)
+        if maximize_intensity:
+            channel_type = numpy.iinfo(channel.dtype)
+            # TODO: better using a threshold? like
+            # image_type.max(image) - image_type.max(image)/100
+            if numpy.max(channel) < channel_type.max:
+                channel = channel.astype(numpy.float64)
+                channel *= channel_type.max / numpy.max(channel)
+                channel = channel.astype(channel_type)
+        channels.append(channel)
+    # remember that opencv precess data as BGR instead of RGB...
+    if maximize_intensity:
+        loginfo = ["channels intensity has been maximized"]
+    else:
+        loginfo = ["original channels intensity"]
+    # TODO: 3 similar repetitions are enought to loop or create a function
+    if "b_channel_multiplier" in kwargs:
+        channels[0] *= kwargs["b_channel_multiplier"]
+        if numpy.max(channels[0]) > numpy.iinfo(channels[0].dtype).max:
+            log.warning("blue channel values are higher than channel depth. Information may be lost")
+        loginfo.append("blue channel multiplied by {num}".format(num = str(kwargs["b_channel_multiplier"])))
+    if "g_channel_multiplier" in kwargs:
+        channels[1] *= kwargs["g_channel_multiplier"]
+        if numpy.max(channels[1]) > numpy.iinfo(channels[1].dtype).max:
+            log.warning("green channel values are higher than channel depth. Information may be lost")
+        loginfo.append("green channel multiplied by {num}".format(num = str(kwargs["g_channel_multiplier"])))
+    if "r_channel_multiplier" in kwargs:
+        channels[2] *= kwargs["r_channel_multiplier"]
+        if numpy.max(channels[2]) > numpy.iinfo(channels[2].dtype).max:
+            log.warning("red channel values are higher than channel depth. Information may be lost")
+        loginfo.append("red channel multiplied by {num}".format(num = str(kwargs["r_channel_multiplier"])))
+    overlay_image = cv2.merge(channels)
+    log.info(", ".join(loginfo))
+
+    # save and/or return rgb_image
+    if "output_file" in kwargs:
+        overlay_file = _validate_filename(kwargs["output_file"], ".png")
+    elif output_folder is not False:
+        if (output_folder is None):
+            output_folder = "."
+        overlay_file = _build_validated_filename(output_folder, "overlay.png")
+    else:
+        overlay_file = None
+    if overlay_file is not None:
+        cv2.imwrite(str(overlay_file), overlay_image)
+        log.info("writing file '{file}'".format(file = str(overlay_file)))
+    if return_image:
+        return overlay_image
 
 
 def highlight_cells(input_folder_rgb = None, input_folder_mask = None,
@@ -177,3 +257,52 @@ def compute_mean(input_folder = None, output_folder = None, return_data = False,
     
     print("compute_mean stub")
     raise NotImplementedError
+
+
+# Auxiliary functions to get/write data
+def _get_filenames_from_folder(folder):
+    """get the `folder` str, checks and returns children as list of Path
+    instances."""
+
+    folder_path = Path(folder)
+    if not folder_path.is_dir():
+        raise ValueError("'{path}' is not a valid directory".format(path=folder))
+    files = os.listdir(folder)
+    if not files:
+        raise ValueError("'{path}' doesn't contain any file".format(path=folder))
+    return [folder_path.joinpath(file) for file in files]
+
+def _get_validated_filenames(*args):
+    """get one or more filename str or list(str), checks validity and returns
+    list of Path instances."""
+
+    files = []
+    for arg in args:
+        if not isinstance(arg, list):
+            arg = [arg]
+        for elem in arg:
+            file = Path(elem)
+            if not file.is_file():
+                raise ValueError("'{elem}' is not a valid file".format(arg=str(arg)))
+            files.append(file)
+    return files
+
+def _build_validated_filename(folder, filename, extension=None):
+    """ check `folder` str validity, attach `filename` str and `extension` str
+    (if provided) and returns Path instance"""
+
+    folder_path = Path(folder)
+    if not folder_path.is_dir():
+        raise ValueError("'{path}' is not a valid directory".format(path=folder))
+    if extension is not None and not filename.endswith(extension):
+        filename += extension
+    return folder_path.joinpath(filename)
+
+def _validate_filename(filename, extension=None):
+    """ check `filename` str validity, attach `extension` str (if provided)
+    and returns Path instance"""
+
+    path = Path(filename)
+    print(path.parent, path.name)
+    return _build_validated_filename(path.parent, path.name, extension)
+
--- tests/image/test_processing.py
+++ tests/image/test_processing.py
@@ -10,8 +10,20 @@ For detailed information please refer to https://readthedocsmissinglink.temp or
 docs/source/tests.rst
 """
 
+import os
 import unittest
+import platform
+import numpy
+import cv2
 from bblab.image import processing
+from pathlib import Path
+
+FOLDER_CHANNELS = "./data/1i_channels"
+FOLDER_OVERLAY = "./data/1o_overlay"
+FOLDER_MASK = "./data/2i_mask"
+FOLDER_HIGHLIGHT = "./data/2o_highlight"
+FOLDER_MEAN = "./data/3i_mean"
+FOLDER_TEMP = "./tests/image"
 
 class TestProcessing(unittest.TestCase):
     def setUp(self):
@@ -22,11 +34,50 @@ class TestProcessing(unittest.TestCase):
 
     # @unittest.expectedFailure
     # also available: @unittest.skip(reason) and @unittest.skipIf(condition, reason)
+    def test_check_images_availability(self):
+        self.assertEqual(len(os.listdir(Path(FOLDER_CHANNELS))), 3,
+                "Channels images not available for tests")
+        self.assertEqual(len(os.listdir(Path(FOLDER_OVERLAY))), 1,
+                "Overlay image not available for tests")
+        # self.assertEqual(len(os.listdir(Path(FOLDER_MASK))), 1,
+        #         "Mask image not available for tests")
+        # self.assertEqual(len(os.listdir(Path(FOLDER_HIGHLIGHT))), 1,
+        #         "Highlight image not available for tests")
+        # self.assertEqual(len(os.listdir(Path(FOLDER_MEAN))), 1,
+        #         "Mean Csv not available for tests")
 
-    @unittest.expectedFailure
+    def test_get_filenames_from_folder(self):
+        filenames = processing._get_filenames_from_folder(FOLDER_CHANNELS)
+        self.assertEqual(len(filenames), 3)
+
+    def test_get_validated_filenames(self):
+        filenames = processing._get_filenames_from_folder(FOLDER_CHANNELS)
+        self.assertEqual(processing._get_validated_filenames(filenames), filenames)
+
+    # TODO: smarter way to test path...
+    def test_build_validated_filename(self):
+        filename_function = processing._build_validated_filename(FOLDER_OVERLAY, "fakename", extension=".tiff")
+        if platform.system() == "Windows":
+            filename_manual = r"data\1o_overlay\fakename.tiff"
+        else:
+            filename_manual = r"data/1o_overlay/fakename.tiff"
+        self.assertEqual(str(filename_function), filename_manual)
+
+    def test_validate_filename(self):
+        filename_function = processing._validate_filename(FOLDER_OVERLAY + "/fakename.tiff")
+        if platform.system() == "Windows":
+            filename_manual = r"data\1o_overlay\fakename.tiff"
+        else:
+            filename_manual = r"data/1o_overlay/fakename.tiff"
+        self.assertEqual(str(filename_function), filename_manual)
+
+    # TODO: using Pillow to load images to be compared? Would it be a double check?
     def test_overlay_channels(self):
-        self.assertRaises(processing.overlay_channels(), "Update the test, it is a placeholder for a not-already-implemented function")
-    
+        image_processed = processing.overlay_channels(FOLDER_CHANNELS, False, return_image = True)
+        files_loaded = processing._get_filenames_from_folder(FOLDER_OVERLAY)
+        image_loaded = cv2.imread(str(files_loaded[0]), cv2.IMREAD_UNCHANGED)
+        self.assertTrue((image_processed == image_loaded).all())
+
     @unittest.expectedFailure
     def test_highlight_cells(self):
         self.assertRaises(processing.highlight_cells(), "Update the test, it is a placeholder for a not-already-implemented function")
